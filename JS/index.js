9a8f92f0c36f897af2140ca78d3b30202e8d4149
Jana May <jana.ricarda.may@gmail.com>
2023-12-14 14:25:41 +0100

    add javaysript

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+const buttonAnswer = document.querySelector('[data-js="answer"]');
+const buttonQuestion = document.querySelector('[data-js="question"]');
+
+buttonQuestion.addEventListener("click", () => {
+  buttonAnswer.classList.toggle("answer");
+});
