a493040a03a8248fa5bd6219c2b57a12c7d22e32
Jana May <jana.ricarda.may@gmail.com>
2023-12-15 14:26:38 +0100

    add javascript

--- JS/index.js
+++ JS/index.js
@@ -2,5 +2,5 @@ const buttonAnswer = document.querySelector('[data-js="answer"]');
 const buttonQuestion = document.querySelector('[data-js="question"]');
 
 buttonQuestion.addEventListener("click", () => {
-  buttonAnswer.classList.toggle("answer");
+  buttonAnswer.classList.toggle("answer--show");
 });
